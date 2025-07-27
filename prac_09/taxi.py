27dbbcc7e1ddfa768e0e348eab77010935ab545d
ratha21 <rishanratha@gmail.com>
2025-07-27 12:38:45 +0530

    Add files via upload

---
+++ prac_09/taxi.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+from car import Car
+
+class Taxi(Car):
+    """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23  # class variable shared by all instances
+
+    price_per_km = 1.23  # class variable
+
+    def __init__(self, name, fuel):
+        super().__init__(name, fuel)
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+
+    def get_fare(self):
+        return round(self.price_per_km * self.current_fare_distance, 1)
+
+    def start_fare(self):
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
